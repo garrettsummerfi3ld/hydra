70b237b253dc448f0cace2de626900aa6f1519af
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2021-01-05 00:01:30 -0600

    Restarting project

---
+++ src/main/java/io/garrettsummerfi3ld/hydra/Hydra.java
@@ -0,0 +1,17 @@
+package io.garrettsummerfi3ld.hydra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hydra extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
